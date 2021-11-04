db4a719b282a5c060f37ac2b4cecdbecacb7fdd8
Kaene K. Lebakeng <kaenekarabolebakeng@gmail.com>
2021-11-04 16:24:20 +0200

    Fixed models

--- account/admin.py
+++ account/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from account.models import Patient
+from account.models import Client, Doctor, Assistant
+
 # Register your models here.
 
 
-admin.site.register(Patient)
+admin.site.register(Client)
+admin.site.register(Doctor)
+admin.site.register(Assistant)
